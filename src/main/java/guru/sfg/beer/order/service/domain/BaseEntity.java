cf793daed8c0913127055777b052eaee506fd63a
Khilna Gudhka <khilnahgudhka@gmail.com>
2025-09-20 16:09:18 +0530

    updating to use snapshot BOM + changed jakarta(javax) packages. Closes #4

--- src/main/java/guru/sfg/beer/order/service/domain/BaseEntity.java
+++ src/main/java/guru/sfg/beer/order/service/domain/BaseEntity.java
@@ -19,12 +19,9 @@ package guru.sfg.beer.order.service.domain;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.GenericGenerator;
-import org.hibernate.annotations.Type;
-import org.hibernate.annotations.UpdateTimestamp;
+import org.hibernate.annotations.*;
 
-import javax.persistence.*;
+import jakarta.persistence.*;
 import java.sql.Timestamp;
 import java.util.UUID;
 
@@ -47,11 +44,8 @@ public class BaseEntity {
 
     @Id
     @GeneratedValue(generator = "UUID")
-    @GenericGenerator(
-            name = "UUID",
-            strategy = "org.hibernate.id.UUIDGenerator"
-    )
-    @Type(type="org.hibernate.type.UUIDCharType")
+    @UuidGenerator
+    //@Type(type="org.hibernate.type.UUIDCharType")
     @Column(length = 36, columnDefinition = "varchar", updatable = false, nullable = false )
     private UUID id;
 
